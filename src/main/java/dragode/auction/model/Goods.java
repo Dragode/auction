4116cf84f2ef6a849add838abae1afc9f1be3af3
Dracode <412507676@qq.com>
2017-03-20 02:18:48 +0800

    添加商品字段

--- src/main/java/dragode/auction/model/Goods.java
+++ src/main/java/dragode/auction/model/Goods.java
@@ -13,9 +13,21 @@ import java.util.Date;
 @Entity
 @Table(name = "goods")
 public class Goods {
+    /**
+     * 等待拍卖
+     */
     public static final Integer WAITING = 0;
+    /**
+     * 拍卖中
+     */
     public static final Integer AUCTION = 1;
+    /**
+     * 拍卖成功
+     */
     public static final Integer DONE = 2;
+    /**
+     * 流拍
+     */
     public static final Integer ABORTIVE = 3;
 
     @Id
@@ -34,6 +46,10 @@ public class Goods {
      */
     private Long startingPrice;
     /**
+     * 市场估价
+     */
+    private Long evaluate;
+    /**
      * 加价幅度
      */
     private Long bidIncrement;
@@ -45,6 +61,18 @@ public class Goods {
      * 延迟周期，X分/次，默认5分/次
      */
     private Integer delayCycle;
+    /**
+     * 规格
+     */
+    private String standard;
+    /**
+     * 品种
+     */
+    private String breed;
+    /**
+     * 作者
+     */
+    private String author;
     /**
      * 拍卖开始时间
      */
@@ -58,7 +86,7 @@ public class Goods {
      */
     private String auctionPic;
     /**
-     * 状态，0：等待拍卖；1：正在拍卖；2：拍卖结束
+     * 状态，0：等待拍卖；1：正在拍卖；2：拍卖结束；3：流拍
      * 便于后台定时任务索引搜索订单
      */
     private Integer status;
@@ -199,7 +227,39 @@ public class Goods {
         this.delayTimes = delayTimes;
     }
 
-    public static Goods newDefaultGoods(){
+    public Long getEvaluate() {
+        return evaluate;
+    }
+
+    public void setEvaluate(Long evaluate) {
+        this.evaluate = evaluate;
+    }
+
+    public String getStandard() {
+        return standard;
+    }
+
+    public void setStandard(String standard) {
+        this.standard = standard;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public static Goods newDefaultGoods() {
         Goods goods = new Goods();
         goods.setStatus(Goods.WAITING);
         goods.setDelayTimes(0);
